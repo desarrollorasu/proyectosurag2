d14df54ee0f77f6240cae0668ac389de45022b57
desarrollorasu <desarrollorasu@gmail.com>
2022-11-23 17:21:45 -0500

    se realizo el controlador JS para medicos

---
+++ src/controllers/controladormedicos.js
@@ -0,0 +1,20 @@
+//etiquetas que controlo:
+let etiquetaNombre=document.getElementById("nombres")
+let etiquetaBoton=document.getElementById("botonenvio")
+let etiquetaDocumento=document.getElementById("documento")
+
+//detectar el evento clic
+etiquetaBoton.addEventListener("click",function(evento){
+    evento.preventDefault()
+    // Swal.fire(
+    //     'Exito!',
+    //     'El especialista ha sido registrado',
+    //     'success'
+    //   )
+    //recibiendo los datos del formulario
+   let nombresMedico=etiquetaNombre.value
+    console.log(nombresMedico)
+    let documentoMedico=etiquetaDocumento.value
+    console.log(documentoMedico)
+
+})
